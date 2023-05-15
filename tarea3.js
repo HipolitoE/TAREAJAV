463d3c851de0652fe56c15f3e66527ce0bb4fb79
HipolitoE <espositohipolito@gmail.com>
2023-05-15 15:26:31 -0300

    correcion con cantidad

--- tarea3.js
+++ tarea3.js
@@ -1,37 +1,39 @@
-class GenerarCarrito {
-    montoTotal = 0;
-    productos = [];
-  
-    constructor(montoTotal, productos) {
-      this.montoTotal = montoTotal;
-      this.productos = productos;
-    }
-  
-    agregarProducto(nombre, precio) {
-      if (this.productos.includes(nombre)) {
-        console.log(`Ya existe el producto ${nombre} en el carrito`);
-      } else {
-        console.log(`* Producto agregado al carrito: ${nombre} - Precio $ ${precio}`);
-        this.productos.push(nombre);
-        this.actualizarMontoTotal(precio);
-      }
-    }
-  
-    actualizarMontoTotal(precio) {
-      this.montoTotal += precio;
-      console.log(`Monto acumulado: $ ${this.montoTotal}`);
+class Carrito {
+  montoTotal = 0;
+  productos = [];
+  cantidad = []
+
+  constructor(montoTotal, productos) {
+    this.montoTotal = 0;
+    this.productos = [];
+    this.cantidad = [];
+  }
+
+  agregarProducto(nombre, precio, cantidad) {
+    if (this.productos.includes(nombre)) {
+      console.log(`Ya existe el producto ${nombre} en el carrito`);
+    } else {
+      console.log(`* Producto agregado al carrito: ${nombre} - Precio $ ${precio} - ${cantidad}`);
+      this.productos.push(nombre);
+      this.actualizarMontoTotal(precio*cantidad);
     }
   }
-  
-  let montoInicial = 0;
-  let productosIniciales = [];
-  let carrito = new GenerarCarrito(montoInicial, productosIniciales);
-  
-  console.log(`El monto Total inicial es de: $ ${carrito.montoTotal} con el producto: ${carrito.productos}`);
-  carrito.agregarProducto("azúcar", 1000);
-  carrito.agregarProducto("Yerba", 500);
-  carrito.agregarProducto("Harina", 400);
-  carrito.agregarProducto("Leche", 400);
-  
-  console.log(`El monto total es de: $ ${carrito.montoTotal}`);
-  console.log(`y los productos son: ${carrito.productos}`);
+
+  actualizarMontoTotal(precio) {
+    this.montoTotal += precio;
+    console.log(`Monto acumulado: $ ${this.montoTotal}`);
+  }
+}
+
+let montoInicial = 0;
+let productosIniciales = [];
+let carrito = new Carrito(montoInicial, productosIniciales);
+
+console.log(`El monto Total inicial es de: $ ${carrito.montoTotal} con el producto: ${carrito.productos}`);
+carrito.agregarProducto("azúcar", 1000, 2);
+carrito.agregarProducto("Yerba", 500, 3);
+carrito.agregarProducto("Harina", 400, 1);
+carrito.agregarProducto("Leche", 400, 2);
+
+console.log(`El monto total es de: $ ${carrito.montoTotal}`);
+console.log(`y los productos son: ${carrito.productos}`);
